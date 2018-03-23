89673d379555d930dd22b0c8cf0ba90663e16fc9
Alexander Olofsson <alexander.olofsson@liu.se>
2018-03-23 10:11:38 +0100

    Handle the args for disk.EnableUUID correctly

--- app/models/concerns/foreman_vmware_advanced/vmware_extensions.rb
+++ app/models/concerns/foreman_vmware_advanced/vmware_extensions.rb
@@ -9,15 +9,14 @@ module ForemanVmwareAdvanced
     def parse_args_with_vmware_advanced(inp_args)
       args = parse_args_without_vmware_advanced(inp_args)
 
-      args[:extra_config] = {
-        'bios.bootOrder'  => 'ethernet0',
-        'svga.autodetect' => 'TRUE'
-      }
+      args[:extra_config] = (args[:extra_config] || {}).merge(
+        'bios.bootOrder'.to_sym  => 'ethernet0',
+        'svga.autodetect'.to_sym => 'TRUE'
+      )
 
-      if args[:compute_attributes] &&
-         args[:compute_attributes][:guest_id] &&
-         args[:compute_attributes][:guest_id].start_with?('win')
-        args[:extra_config]['disk.enableUUID'] = 'TRUE'
+      if args[:guest_id] &&
+         args[:guest_id].start_with?('win')
+        args[:extra_config]['disk.EnableUUID'.to_sym] = 'TRUE'
       end
 
       args
